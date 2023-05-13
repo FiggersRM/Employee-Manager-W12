7a2741b7afcfcf928075a9d2e23f8c0467faf2a5
FiggersRM <MitchFiggers@gmail.com>
2023-05-12 20:29:14 -0400

    Updated the handler functions in the index file as well as the functions in the query file to update the database. Still have to do update the employee role

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
 const cTable = require('console.table');
 const Queries = require('./queryFunctions');
-
-
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'employeeManager_db'
-    },
-    console.log('Connected to the employeeManager_db database')
-);
 
 function selectUse() {
     inquirer
@@ -33,6 +21,12 @@ function selectUse() {
             handleViewRoles();
         } else if (data.choice === 'View all employees') {
             handleViewEmployees();
+        } else if (data.choice === 'Add a department') {
+            handleAddDepartment();
+        } else if (data.choice === 'Add a role') {
+            handleAddRole();
+        } else if (data.choice === 'Add an employee') {
+            handleAddEmployee();
         }
     })
 }
@@ -56,15 +50,83 @@ function handleViewEmployees() {
 }
 
 function handleAddDepartment() {
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: 'What is the name of the department you would like to add?',
+            name: 'addedDepartment'
+        }
+    ])
+    .then((response) => {
+        data = response;
+        Queries.addDepartment(data.addedDepartment);
+        const tableData = Queries.viewDepartments();
+        console.table(tableData);
+        selectUse();
+    })
     
 }
 
 function handleAddRole() {
-    
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: 'What is the title of the role you would like to add?',
+            name: 'addedRole'
+        },
+        {
+            type: 'number',
+            message: 'What is the salary of the new role?',
+            name: 'roleSalary'
+        },
+        {
+            type: 'number',
+            message: 'What is the id of the department for the new role?',
+            name: 'departmentId'
+        }
+    ])
+    .then((response) => {
+        data = response;
+        Queries.addRole(data.addedRole, data.roleSalary, data.departmentId);
+        const tableData = Queries.viewRoles();
+        console.table(tableData);
+        selectUse();
+    })
 }
 
 function handleAddEmployee() {
-    
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: 'What is the first name of the employee?',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the last name of the new employee?',
+            name: 'lastName'
+        },
+        {
+            type: 'number',
+            message: 'What is the role id of the new employee?',
+            name: 'roleId'
+        },
+        {
+            type: 'number',
+            messaage: 'What is the manager id of the new employee, if applicable',
+            name: 'managerId'
+        }
+    ])
+    .then((response) => {
+        data = response;
+        Queries.addEmployee(data.firstName, data.lastName, data.roleId, data.managerId);
+        const tableData = Queries.viewEmployees();
+        console.table(tableData);
+        selectUse();
+    })
 }
 
 function handleUpdateRole() {
--- queryFunctions.js
+++ queryFunctions.js
@@ -12,21 +12,39 @@ const db = mysql.createConnection(
 
 class Queries {
     viewDepartments () {
+        console.log('view departments query');
         db.query('SELECT * from departments', function (err, results) {
             return results;
         });
     }
 
     viewRoles() {
+        console.log('view roles query');
         db.query('SELECT * from roles', function (err, results) {
             return results;
         });
     }
 
     viewEmployees() {
+        console.log('view employees query');
         db.query('SELECT * from employees', function (err, results) {
             return results;
         });
+    }
+
+    addDepartment (name) {
+        db.query(`INSERT INTO departments (name) VALUES (${name});`);
+        console.log('Department added successfully');
+    }
+
+    addRole (title, salary, departmentId) {
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES (${title}, ${salary}, ${departmentId});`);
+        console.log('Role added successfully');
+    }
+
+    addEmployee (firstName, lastName, roleId, managerId) {
+        db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (${firstName}, ${lastName}, ${roleId}, ${managerId});`);
+        console.log('Employee added successfully');
     }
 }
 
